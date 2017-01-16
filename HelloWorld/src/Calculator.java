3a03b62f4b4cedf7e71138fe106a67a4329a96fe
Alex Tsang <Alex Tsang@AlexTsang_Dell>
2017-01-16 17:26:03 +0800

    Add calculator

---
+++ HelloWorld/src/Calculator.java
@@ -0,0 +1,28 @@
+
+public class Calculator {
+
+	private double number1;
+	private double number2;
+	
+	public Calculator(double number1, double number2) {
+		super();
+		this.number1 = number1;
+		this.number2 = number2;
+	}
+	
+	public double add(){
+		return number1 + number2;
+	}
+	
+	public double sub(){
+		return number1 - number2;
+	}
+	
+	public double times(){
+		return number1 * number2;
+	}
+	
+	public double over(){
+		return number1 / number2;
+	}
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Alex");
+		
+		Calculator calculator = new Calculator (1,2); 
+		System.out.println(calculator.over());
+		
 	}
 
 }
